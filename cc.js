69041ca883cd3d71af280bf25cef88f061711730
nogg3r5 <nogg3r5@gmail.com>
2021-03-13 20:37:07 +0000

    This one actually does things

--- cc.js
+++ cc.js
@@ -1,10 +1,11 @@
 var made = 0
 var drunk = 0
 function MakeCoffee(number){
-    made = made + number;
+ made = made + number;
+ document.getElementById("made").innerHTML = made;
 };
 
 function DrinkCoffee(number){
-  if (drunk < made){drunk = drunk + number;}
-  else{console.log('You need to make a drink, you have only made '+made);}
+  if (drunk < made){drunk = drunk + number;document.getElementById("drunk").innerHTML = drunk;}
+  else{document.getElementById("errors").innerHTML = 'You need to make a drink!';}
 };
